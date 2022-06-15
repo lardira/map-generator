d65fe5bf7f3f29e15cdec26b87b8ddca79633303
lardira <danilkefimov@gmail.com>
2022-06-15 21:40:36 +0300

    changed block colors

--- MapGenerator/src/MapView.cpp
+++ MapGenerator/src/MapView.cpp
@@ -6,6 +6,7 @@ MapView::MapView(QWidget* parent)
 	setScene(mapScene);
 
 	mapScene->setBackgroundBrush(bgColor);
+
 	//TODO:concurrent drawing on the qimage (map) and place it into the scene
 
 	this->show();
@@ -16,22 +17,22 @@ QColor MapView::BlockTypeColor(const BlockType& type) const
 	switch (type)
 	{
 	case BlockType::DeepOcean:
-		return Qt::darkBlue;
+		return QColor{ 40, 72, 176, 220 }; //kinda dark blue
 		break;
 	case BlockType::CloseOcean:
-		return Qt::blue;
+		return QColor{ 15, 64, 224, 88 }; //kinda simple blue
 		break;
 	case BlockType::Sand:
-		return QColor{ 252, 206, 0 }; //kinda yellow
+		return QColor{ 224, 200, 38 }; //kinda yellow
 		break;
 	case BlockType::Ground:
-		return QColor{ 97, 255, 97 }; //kinda green
+		return QColor{ 4, 224, 117 }; //kinda green
 		break;
 	case BlockType::Hill:
-		return Qt::gray;
+		return QColor{ 120, 120, 120 };
 		break;
 	case BlockType::Mountain:
-		return Qt::darkGray;
+		return QColor{ 64, 64, 64 };
 		break;
 	default:
 		return Qt::black;
